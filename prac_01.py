bc94114f5aa2204bfb96db179fcb8c82a7851105
Bhatt-kapil <161840676+Bhatt-kapil@users.noreply.github.com>
2025-02-04 22:19:18 +0530

    Add files via upload

---
+++ prac_01.py
@@ -0,0 +1,8 @@
+
+pi = 3.14159
+
+radius = float(input("Enter the radius of the circle: "))
+
+area = pi * (radius ** 2)
+
+print(f"The area of the circle with radius {radius} is: {area:.2f}")
---
+++ prac_02.py
@@ -0,0 +1,8 @@
+import random
+import string
+
+characters = string.ascii_letters + string.digits + string.punctuation
+
+password = ''.join(random.choice(characters) for _ in range(8))
+
+print(f"Your generated password is: {password}")
